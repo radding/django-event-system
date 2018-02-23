986ab701c7ac27d9feb1739a52ebf9ec3abc01e9
Yoseph Radding <yoseph@shuttl.io>
2018-02-23 08:16:03 -0500

    Updated suppported python version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'greenlet',
         'pytz',
     ],
-    python_requires='>=3',
+    python_requires='>=3.4',
     keywords="django events events for django",
     classifiers=[
         'Environment :: Web Environment',
